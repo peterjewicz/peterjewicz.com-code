f9331d94e9014776f881c616b1b1611029dbea66
peterjewicz <peterjewicz@totalwebconnections.com>
2022-12-26 15:29:06 -0600

    CLJS dialogue article code

---
+++ samples/dialogue_demo.cljs
@@ -0,0 +1,60 @@
+(ns dialogue-demo.views
+  (:require
+   [reagent.core :as r]
+   [re-frame.core :as re-frame]
+   [dialogue-demo.subs :as subs]
+   ))
+
+;; {:key {:description "" :options [:text "" :constraints [] :effects []]}}
+
+;; {:base [{:text "Here's a test description"
+;;          :options [{:display "Go Foward" :action [{:type :key :value :second}]}]}]}
+
+
+;; In a real application you'd want to have this either at the component level 
+;; or, more likely, stored in a state DB
+(def !current-step (r/atom :base))
+
+(def dialogue 
+  {:base {:description "First description"
+          :options [{:display "Go Forward" :actions [{:type :key :value :second} {:type :quest :name "Fetch Quest"}]}
+                    {:display "Third" :actions [{:type :key :value :third}]}]} 
+   :second {:description "Second"
+            :options [{:display "Leave" :actions [{:type :key :value :base}]}
+                      {:display "Leave With Gold" :actions [{:type :key :value :base}{:type :gold :amount 20}]}]}
+   :third {:description "Now You're On The Third option."
+           :options [{:constraint true 
+                      :false {:display "You Don't Have it"} 
+                      :true {:display "You Do have It"}}]}})
+
+(defn check-constraint []
+  true)
+
+(defmulti handle-dialogue-action :type)
+
+; In this example we'll reset the atom manually, but you'd probably want to setup 
+; event/effect handlers for these operations
+(defmethod handle-dialogue-action :key [action]
+  (reset! !current-step (:value action)))
+
+(defmethod handle-dialogue-action :quest [action]
+  (js/alert (str "You started quest: " (:name action))))
+
+(defmethod handle-dialogue-action :gold [action]
+  (js/alert (str "You got: " (:amount action) " gold")))
+
+(defn render-button [option]
+  (let [display (:display option)]
+    [:button {:on-click #(doseq [action (:actions option)] (handle-dialogue-action action)) :key display}
+     display]))
+
+(defn Dialogue-view [{:keys [description options]} dialogue-step]
+  [:div
+   [:p description]
+   (for [option options]
+     (if (:constraint option)
+       (render-button ((-> (check-constraint) str keyword) option))
+       (render-button option)))])
+
+(defn main-panel [] 
+  [Dialogue-view (@!current-step dialogue)])
